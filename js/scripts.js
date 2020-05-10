110789464f8310581ad24ae70ebd3c9256da1772
Frank <kipfrankline@gmail.com>
2020-05-11 00:10:04 +0300

    Add initial changes

--- js/scripts.js
+++ js/scripts.js
@@ -4,82 +4,82 @@ $(document).ready(function(){
     $("#design").show();
   });
   $("#design").click(function(){
-    $("#design").slideUp('1500');
-    $("#design-image").slideDown('1500');
+    $("#design").slideUp();
+    $("#design-image").slideDown();
   });
 });
 $(document).ready(function(){
     $("#development-image").click(function(){
       $("#development-image").slideDown().hide();
-      $("#development").show('1500');
+      $("#development").show();
     });
     $("#development").click(function(){
-      $("#development").slideUp('1500');
-      $("#development-image").slideDown('1500');
+      $("#development").slideUp();
+      $("#development-image").slideDown();
     });
   });
   $(document).ready(function(){
     $("#product-image").click(function(){
-      $("#product-image").slideDown('1500').hide('1000');
-      $("#product").show('1500');
+      $("#product-image").slideDown().hide();
+      $("#product").show();
     });
     $("#product").click(function(){
-      $("#product").slideUp('1500');
-      $("#product-image").slideDown('1500');
+      $("#product").slideUp();
+      $("#product-image").slideDown();
     });
   });
   
   $(document).ready(function(){
-    $("#work1").mouseover(function(){
+    $("#work").mouseover(function(){
       $("#overlay").show();
     }).mouseout(function(){
       $("#overlay").hide();
     });
   });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#overlay4").show();
-    }).mouseout(function(){
-      $("#overlay4").hide();
-    });
-  });
+  // $(document).ready(function(){
+  //   $("#work2").mouseover(function(){
+  //     $("#overlay2").show();
+  //   }).mouseout(function(){
+  //     $("#overlay2").hide();
+  //   });
+  // });
+  // $(document).ready(function(){
+  //   $("#work3").mouseover(function(){
+  //     $("#overlay3").show();
+  //   }).mouseout(function(){
+  //     $("#overlay3").hide();
+  //   });
+  // });
+  // $(document).ready(function(){
+  //   $("#work4").mouseover(function(){
+  //     $("#overlay4").show();
+  //   }).mouseout(function(){
+  //     $("#overlay4").hide();
+  //   });
+  // });
   
-  $(document).ready(function(){
-    $("#work5").mouseover(function(){
-      $("#overlay5").show();
-    }).mouseout(function(){
-      $("#overlay5").hide();
-    });
-    $("#work6").mouseover(function(){
-      $("#overlay6").show();
-    }).mouseout(function(){
-      $("#overlay6").hide();
-    });
-    $("#work7").mouseover(function(){
-      $("#overlay7").show();
-    }).mouseout(function(){
-      $("#overlay7").hide();
-    });
-    $("#work8").mouseover(function(){
-      $("#overlay8").show();
-    }).mouseout(function(){
-      $("#overlay8").hide();
-    });
-  });
+  // $(document).ready(function(){
+  //   $("#work5").mouseover(function(){
+  //     $("#overlay5").show();
+  //   }).mouseout(function(){
+  //     $("#overlay5").hide();
+  //   });
+  //   $("#work6").mouseover(function(){
+  //     $("#overlay6").show();
+  //   }).mouseout(function(){
+  //     $("#overlay6").hide();
+  //   });
+  //   $("#work7").mouseover(function(){
+  //     $("#overlay7").show();
+  //   }).mouseout(function(){
+  //     $("#overlay7").hide();
+  //   });
+  //   $("#work8").mouseover(function(){
+  //     $("#overlay8").show();
+  //   }).mouseout(function(){
+  //     $("#overlay8").hide();
+  //   });
+  // });
   
   $(document).ready(function(){
     $("form#message-form").submit(function(){
