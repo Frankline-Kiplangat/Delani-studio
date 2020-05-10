aecc650dbe792eff2db317d35a9f7ddddaa741a8
Frank <kipfrankline@gmail.com>
2020-05-11 00:03:35 +0300

    Modifed jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
+  $("#design-image").click(function(){
+    $("#design-image").slideDown().hide();
+    $("#design").show();
+  });
+  $("#design").click(function(){
+    $("#design").slideUp('1500');
+    $("#design-image").slideDown('1500');
+  });
+});
+$(document).ready(function(){
     $("#development-image").click(function(){
-      $("#development-image").slideDown('1500').hide('1000');
+      $("#development-image").slideDown().hide();
       $("#development").show('1500');
     });
     $("#development").click(function(){
@@ -8,18 +18,6 @@ $(document).ready(function(){
       $("#development-image").slideDown('1500');
     });
   });
-  
-  $(document).ready(function(){
-    $("#design-image").click(function(){
-      $("#design-image").slideDown('1500').hide('1000');
-      $("#design").show('1500');
-    });
-    $("#design").click(function(){
-      $("#design").slideUp('1500');
-      $("#design-image").slideDown('1500');
-    });
-  });
-  
   $(document).ready(function(){
     $("#product-image").click(function(){
       $("#product-image").slideDown('1500').hide('1000');
