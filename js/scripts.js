08554433852a9f2dcc405c0a5fefd76e6f213e84
Frank <kipfrankline@gmail.com>
2020-05-11 23:16:57 +0300

    Added styling

--- js/scripts.js
+++ js/scripts.js
@@ -90,11 +90,11 @@ $(document).ready(function(){
     $("form#message-form").submit(function(){
       var name = $("input#input1").val();
       if ($("input#input1").val() && $("input#input2").val()){
-        alert(name + ",thank you for reaching out to us 😉. We will get back to you soon");
+        alert(name + ",thank you for reaching out to us 😉. We will get back to you soon🤝.");
       }
-      // else {
-      //   alert("Please enter your name and email address!");
-      // }
+      else {
+        alert("Please enter your name and email address!");
+      }
       
     });
   
